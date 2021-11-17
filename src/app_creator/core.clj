c211864cb64537028e04a87eb99595eb99ec47d7
Ekaterina Golikova <moskva20013@gmail.com>
2021-11-17 18:10:23 +0300

    Console ui works with clojure.tools.cli

--- src/app_creator/core.clj
+++ src/app_creator/core.clj
@@ -3,9 +3,9 @@
 (require '[app-creator.ui.cli :as ui])
 
 (defn -main [& args]
+  ;(println (str "\nmain. args: " args))
   (try
     (ui/run args)
-    ;(println (re-matches #"^(.+)(\/|\\)([^(\/|\\)]+)(\.yaml|\.yml)$" "C:\\Users\\Lenovo X1\\Downloads\\yaml.ml"))
      (catch Exception e
        (println "Exception message: " (.getMessage e))
        ))
--- src/app_creator/ui/cli.clj
+++ src/app_creator/ui/cli.clj
@@ -1,43 +1,39 @@
 (ns app-creator.ui.cli)
 
-(require '[clojure.tools.cli :as tools]
+(require '[clojure.tools.cli :exclude boolean? :as cli-tools]
          '[clojure.string :as string]
          '[clojure.java.io :as io]
          '[app-creator.parser.parser :as parser])
 
-;(def path-in-regex #"^(.+)(\/|\\)([^(\/|\\)]+)(\.yaml|\.yml)$")
-(def path-in-regex #"^(.+)(\.yaml|\.yml)$")
-(def path-out-regex #"^([^\.])+$")
-(def path-in nil)
-(def path-out nil)
+(def in-path-regex #"^(.+)(\.yaml|\.yml)$")
+(def out-path-regex #"^([^\.])+$")
+(defn required-opt-errs [opt-name]
+  [(str "Invalid options number: " opt-name " option must be specified.")])
 
-(defn start [path-in path-out]
-  (parser/parse-from-file path-in))
+; C:/Users/Lenovo X1/Downloads/yaml.yaml
+; C:\Users\Lenovo X1\Downloads\yaml.yaml
 
 (def cli-options
-  [["-i" "--path-in PATH-IN" "Absolute path to .yml/.yaml-file with description"
-    ;:validate [#(some? (re-matches path-in-regex %)) "Must be correct absolute path to .yml/.yaml-file."]
-    :validate [#(and (.exists (io/file %)) (some? (re-matches path-in-regex %)))
-               "Must be correct absolute path to existing .yml/.yaml-file."]]
-   ["-o" "--path-out PATH-OUT" "Absolute path to directory where the result will be"
-    :validate [#(and (.isDirectory (io/file %)) (some? (re-matches path-out-regex %)))
-               "Must be correct absolute path to directory."]]
-   ["-q" "--quit" "Quit from app"]
+  [["-i" "--in-path IN-PATH" "Absolute path to .yml/.yaml-file with description"
+    :validate [#(and (.exists (io/file %)) (some? (re-matches in-path-regex %)))
+               "Must be correct absolute path to existing .yml/.yaml-file with quotes."]]
+   ["-o" "--out-path OUT-PATH" "Absolute path to directory where the result will be"
+    :validate [#(and (.isDirectory (io/file %)) (some? (re-matches out-path-regex %)))
+               "Must be correct absolute path to directory with quotes."]]
    ["-h" "--help"]])
 
 (defn usage [options-summary]
-  (->> ["This is my program. There are many like it, but this one is mine."
+  (->> ["Hello!"
         ""
-        "Usage: program-name [options] action"
+        "Usage: program-name action [options]"
         ""
         "Options:"
         options-summary
         ""
         "Actions:"
         "  start    Start app"
-        "  exit     Exit app"
         ""
-        "Please refer to the manual page for more information."]
+        "You have to specify both path to source file and path to target directory to start."]
        (string/join \newline)))
 
 (defn error-msg [errors]
@@ -49,7 +45,19 @@
   should exit (with an error message, and optional ok status), or a map
   indicating the action the program should take and the options provided."
   [args]
-  (let [{:keys [options arguments errors summary]} (tools/parse-opts args cli-options)]
+  ;(println (str "\nvalidate-args. unparsed args: " args))
+  (let [{:keys [options arguments errors summary]} (cli-tools/parse-opts args cli-options)]
+    ;(println (str "\nvalidate-args. parsed-opts:\noptions: " options
+    ;              "\narguments: " arguments "\nerrors: " errors "\nsummary: " summary))
+    ;(println "")
+
+    ;(try
+    ;  (assert (:in-path options) "`--in-path` must be specified!")
+    ;  (assert (:out-path options) "`--out-path` must be specified!")
+    ;  (catch AssertionError e
+    ;    (println "Invalid options number: " (.getMessage e))
+    ;    ))
+
     (cond
       ; если в options есть help, то возвращаем мапу:
       ; exit-message=описание и "всё ли ок"=true
@@ -62,11 +70,20 @@
       {:exit-message (error-msg errors)}
 
       ;; custom validation on arguments
-      ; если валидация аргументов проходит (он 1 и равен start либо exit),
+      ; если валидация аргументов проходит (он 1 и равен start либо exit)
+      ; и если есть обе необходимые опции (указаны оба пути),
       ; то возвращаем мапу, что нужно действие с таким-то аргументом и такими опциями
       (and (= 1 (count arguments))
            (#{"start" "exit"} (first arguments)))
-      {:action (first arguments) :options options}
+      (cond
+         (not (:in-path options))
+         {:exit-message (error-msg (required-opt-errs "--in-path"))}
+
+         (not (:out-path options))
+         {:exit-message (error-msg (required-opt-errs "--out-path"))}
+
+         :else
+         {:action (first arguments) :options options})
 
       ; иначе считаем что пользователь тупой и просто выводим ему саммари
       :else
@@ -76,15 +93,15 @@
   (println msg)
   (System/exit status))
 
-(defn run [& args]
+(defn start [path-in path-out]
+  (parser/parse-from-file path-in))
+
+(defn run [args]
+  ;(println (str "\nrun. args: " args))
   (let [{:keys [action options exit-message ok?]} (validate-args args)]
-    (println (str "options: " options "."))
+    ;(println (str "\nrun. validated args: " [action options exit-message ok?]))
     (if exit-message
-      (exit (if ok? 0 1) exit-message)                      ; не оч поняла, зачем тут выходить
+      (exit (if ok? 0 1) exit-message)
       (case action
-        "start" (start (first options) (second options))
-        "exit" (exit 0 "Bye!")))))
+        "start" (start (:in-path options) (:out-path options))))))
 
-
-;(defn run [& args]
-;  (tools/parse-opts args cli-options))
