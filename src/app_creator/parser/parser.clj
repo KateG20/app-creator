b8becbd6b19c750c9ffc005bfb67ac60cf314f65
Ekaterina Golikova <moskva20013@gmail.com>
2021-12-08 18:04:04 +0300

    Cleaned

--- src/app_creator/parser/parser.clj
+++ src/app_creator/parser/parser.clj
@@ -1,16 +1,9 @@
 (ns app-creator.parser.parser)
 
-(require '[clj-yaml.core :as yaml]
-         '[defun.core :refer [defun]])
+(require '[clj-yaml.core :as yaml])
 
 (defn parse-from-file [path]
-  ;(yaml/parse-string (slurp path))
-  (println (yaml/parse-string (slurp path)))
-  )
+  (yaml/parse-string (slurp path)))
 
-(defun get-info-about
-         (["db"] (println 'db))
-         (["server"] (println 'back))
-         ([other] (throw (Throwable. (format "Incorrect name of app part: %s", other))))
-  )
+; todo semantic validation
 
