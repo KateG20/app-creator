b5c808ea45a406a8a32a177c1907f2d0ac90bfcd
Ekaterina Golikova <moskva20013@gmail.com>
2021-12-08 18:02:51 +0300

    Added ns with global creator

---
+++ src/app_creator/creator/creator.clj
@@ -0,0 +1,12 @@
+(ns app-creator.creator.creator)
+
+(require '[app-creator.parser.parser :as parser]
+         '[app-creator.creator.db :as db])
+
+(defn create [in-path out-path]
+  (let [{:keys [info db server front]} (parser/parse-from-file in-path)]
+    (db/create db out-path)
+    ;(server/create server out-path)
+    ;(front/create front out-path)
+    )
+  )
