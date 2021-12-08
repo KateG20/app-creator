fda11f2a4b7b5d5b7683bd10c4486b978d20a5eb
Ekaterina Golikova <moskva20013@gmail.com>
2021-12-08 18:00:33 +0300

    Updates

--- src/app_creator/core.clj
+++ src/app_creator/core.clj
@@ -3,13 +3,9 @@
 (require '[app-creator.ui.cli :as ui])
 
 (defn -main [& args]
-  ;(println (str "\nmain. args: " args))
   (try
     (ui/run args)
      (catch Exception e
        (println "Exception message: " (.getMessage e))
        ))
-  ;(println "App run")
   )
-
-;(-main)
