6bfbead1e7e1cfbd247d274880502ec053ba37f6
Ekaterina Golikova <moskva20013@gmail.com>
2022-01-12 17:11:27 +0300

    String templates for code and script generation

--- src/app_creator/creator/templates.clj
+++ src/app_creator/creator/templates.clj
@@ -2,14 +2,26 @@
   (:import (java.io File)))
 (require '[clojure.string :as string])
 
-(defn sql-bat-templ [pwd host user db-name path]
+(defn sql-bat [pwd host user db-name path]
   (as-> ["@echo off"
-        ""
-        "setlocal"
-        "set PGPASSWORD=%s"
-        "psql -h %s -U %s -f \"%s%screateDB.sql\""
-        "psql -h %s -U %s -d %s -f \"%s%screateTables.sql\""
-        "endlocal"
-        "pause"] $
-       (string/join \newline $)
-       (format $ pwd host user path File/separator host user db-name path File/separator)))
+         ""
+         "setlocal"
+         "set PGPASSWORD=%s"
+         "psql -h %s -U %s -f \"%s%screateDB.sql\""
+         "psql -h %s -U %s -d %s -f \"%s%screateTables.sql\""
+         "endlocal"
+         "pause"] $
+        (string/join \newline $)
+        (format $ pwd host user path File/separator host user db-name path File/separator)))
+
+(defn create-db [db-name]
+  (-> "SELECT 'CREATE DATABASE %s' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '%s')\\gexec"
+      (format db-name db-name)))
+
+(defn create-table [table-name cols]
+  (as-> ["CREATE TABLE %s ("
+         "%s);"
+         ""
+         ""] $
+        (string/join \newline $)
+        (format $ table-name cols)))
