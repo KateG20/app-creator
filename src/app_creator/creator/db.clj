5e9fb8d87f32ced3877d664496d73edb453fc63f
Ekaterina Golikova <moskva20013@gmail.com>
2022-01-12 17:10:51 +0300

    Namespace for generating sql scripts for db creating

--- src/app_creator/creator/db.clj
+++ src/app_creator/creator/db.clj
@@ -1,26 +1,38 @@
 (ns app-creator.creator.db
   (:import (java.io File)))
 
-(require '[clojure.java.jdbc :as sql]
-         '[app-creator.creator.templates :as tmpl])
+(require '[app-creator.creator.templates :as tmpl])
 
-(defn create-DB-script [specs out-path]
-  (let [{:keys [name host username password]} specs]
-    (spit (format "%s%ssql.bat" out-path File/separator)
-          (tmpl/sql-bat-templ password host username name out-path))))
+(defn psql-script [db-name host username password out-path]
+  (spit (format "%s%ssql.bat" out-path File/separator)
+        (tmpl/sql-bat password host username db-name out-path)))
 
-(defn create-tables-script [specs out-path]
-  )
+(defn create-DB-script [db-name out-path]
+  (spit (format "%s%screateDB.sql" out-path File/separator)
+        (tmpl/create-db db-name)))
+
+(defn get-columns-info [m]
+  "Достает из мапы данные в формате 'имя_колонки параметры_колонки' "
+  (apply str
+         (for [[k v] (:columns m)]
+           (format "\t%s %s,\n" (name k) v))))
+
+(defn create-table-script [m]
+  "Запихивает данные о колонках в create-table DDL"
+  (apply str
+         (for [[k v] m]
+           (tmpl/create-table (name k) (get-columns-info v)))))
+
+(defn create-tables-script [tables out-path]
+  (spit (format "%s%screateTables.sql" out-path File/separator)
+        (create-table-script tables)))
 
 (defn create [specs out-path]
-  (create-DB-script specs out-path))
-
-(defn create-db [name host user]
-  (sql/create-table-ddl
-    :shouts
-    [[:id :serial "PRIMARY KEY"]
-     [:body :varchar "NOT NULL"]
-     [:created_at :timestamp
-      "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]])
+  "Создает скрипты для создания базы данных и таблиц и скрипт для их выполнения"
+  (let [{:keys [name host username password tables]} specs]
+    (psql-script name host username password out-path)
+    (create-DB-script name out-path)
+    (create-tables-script tables out-path))
   )
+
 
