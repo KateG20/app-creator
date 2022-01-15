f3ef446b73b4dd515966d72522b6f735266396a6
Ekaterina Golikova <moskva20013@gmail.com>
2022-01-16 00:37:19 +0300

    Started server creation part

--- src/app_creator/creator/creator.clj
+++ src/app_creator/creator/creator.clj
@@ -1,12 +1,13 @@
 (ns app-creator.creator.creator)
 
 (require '[app-creator.parser.parser :as parser]
-         '[app-creator.creator.db :as db])
+         '[app-creator.creator.db :as db]
+         '[app-creator.creator.server :as server])
 
 (defn create [in-path out-path]
   (let [{:keys [info db server front]} (parser/parse-from-file in-path)]
-    (db/create db out-path)
-    ;(server/create server out-path)
+    ;(db/create db out-path)
+    (server/create server out-path)
     ;(front/create front out-path)
     )
   )
