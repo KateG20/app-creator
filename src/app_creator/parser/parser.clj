e616db684a7e0423deb32ffd3c0a581f39bf073b
Ekaterina Golikova <moskva20013@gmail.com>
2022-02-18 20:22:21 +0300

    Added semantic validation

--- src/app_creator/parser/parser.clj
+++ src/app_creator/parser/parser.clj
@@ -1,9 +1,11 @@
 (ns app-creator.parser.parser)
 
-(require '[clj-yaml.core :as yaml])
+(require '[clj-yaml.core :as yaml]
+         '[app-creator.parser.validator :as validator])
 
 (defn parse-from-file [path]
-  (yaml/parse-string (slurp path)))
 
-; todo semantic validation
+  (let [text (yaml/parse-string (slurp path))]
+    (if (validator/validate text) (println "VALID") (println "INVALID")) ;TODO норм сообщение, если валидация не прошла
+    text))
 
