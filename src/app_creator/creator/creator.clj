0329881535b97cb85851800be23380b9fdc90220
Ekaterina Golikova <moskva20013@gmail.com>
2022-03-04 13:28:24 +0300

    Added adapter using

--- src/app_creator/creator/creator.clj
+++ src/app_creator/creator/creator.clj
@@ -2,12 +2,20 @@
 
 (require '[app-creator.parser.parser :as parser]
          '[app-creator.creator.db :as db]
-         '[app-creator.creator.server :as server])
+         '[app-creator.creator.server :as server]
+         '[app-creator.creator.adapter :as adapter])
 
 (defn create [in-path out-path]
   (let [{:keys [info db server front]} (parser/parse-from-file in-path)]
-    ;(db/create db out-path)
-    (server/create server out-path)
+    (and (some? db)
+         (let [{:keys [type]} (db)]
+           (adapter/db-type-adapter type db out-path)))
+
+    (and (some? server)
+         (let [{:keys [type]} (server)]
+           (adapter/server-type-adapter type server out-path)))
+
     ;(front/create front out-path)
     )
+  (println "created")
   )
