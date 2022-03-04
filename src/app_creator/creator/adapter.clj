96ed3295ec324f0ec8d027ff5546c87b039d33f5
Ekaterina Golikova <moskva20013@gmail.com>
2022-03-04 16:48:09 +0300

    Added server-lang-adapter

--- src/app_creator/creator/adapter.clj
+++ src/app_creator/creator/adapter.clj
@@ -1,20 +1,33 @@
 (ns app-creator.creator.adapter)
 
-(require '[app-creator.creator.db.postgresql.db :as postgresql]
-         '[app-creator.creator.server.spring.server :as spring])
+(require '[app-creator.creator.db.postgresql.setter :as postgresql]
+         '[app-creator.creator.server.spring.setter :as spring]
+         '[app-creator.creator.server.spring.filler.java :as java-fill])
 
 (defn db-type-adapter [type specs out-path]
   (cond
     (= type "postgresql")
     (postgresql/create specs out-path)
 
-    ; add conditions for other types here
-    ))
+    ; add redirections for other types here
+    )
+  )
 
 (defn server-type-adapter [type specs out-path]
   (cond
     (= type "spring")
     (spring/create specs out-path)
 
-    ; add conditions for other types here
-    ))
+    ; add redirections for other types here
+    )
+  )
+
+(defn server-lang-adapter [lang specs out-path]
+  (cond
+    (= lang "java")
+    (java-fill/fill-controllers specs out-path)
+
+    ; add redirections for other types here
+    )
+  )
+
