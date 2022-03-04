a31dfe82f79ca2afa6f03ffdac3063e584b5e091
Ekaterina Golikova <moskva20013@gmail.com>
2022-03-04 16:51:05 +0300

    Added path to server packages constructor

--- src/app_creator/creator/server/spring/templates.clj
+++ src/app_creator/creator/server/spring/templates.clj
@@ -1,9 +1,17 @@
 (ns app-creator.creator.server.spring.templates
   (:import (java.io File)))
 
+(require '[clojure.string :as string])
+
 (defn spring-init [name options path]
   (as-> ["@echo off"
          ""
          "spring init %s\"%s%s%s\""] $
         (string/join \newline $)
         (format $ options path File/separator name)))
+
+(defn path-to-packages [out-path name group artifact lang]
+  (cond
+    (= lang "java")
+    (format "%s/%s/src/main/java/%s/%s/" out-path name (string/replace group #"\." "/") artifact))
+  )
