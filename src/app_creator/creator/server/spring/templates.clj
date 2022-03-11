10211b0857061515c0a6db733c7f514f77872d5b
Ekaterina Golikova <moskva20013@gmail.com>
2022-03-11 15:35:23 +0300

    Added controller template

--- src/app_creator/creator/server/spring/templates.clj
+++ src/app_creator/creator/server/spring/templates.clj
@@ -16,12 +16,44 @@
           (<< $)))
   )
 
-(defn path-to-packages [out-path name group artifact lang]
+(defn path-to-packages [out-path proj-name group artifact lang]
   (cond
     (= lang "java")
     (let [group (string/replace group #"\." "/")]
-      (-> "{{out-path}}/{{name}}/src/main/java/{{group}}/{{artifact}}/"
+      (-> "{{out-path}}/{{proj-name}}/src/main/java/{{group}}/{{artifact}}/"
           (string/replace "/" "{{sep}}")
           (<<)))
     )
+  )
+
+(defn service-name [controller-name]
+  (as-> controller-name $
+        (count $)
+        (- $ 10)
+        (subs controller-name 0 $)
+        (string/lower-case $)
+        (str $ "Service")))
+
+(defn request [req-name uri type]
+  (->> [""]
+       (string/join \newline)
+       (<<))
+  )
+
+(defn controller [group proj-name controller-name requests]
+  (let [service-class-name (string/capitalize (service-name controller-name))
+        service-var-name (service-name controller-name)]
+    (->> ["package {{group}}.{{proj-name}}.controller;"
+          ""
+          "import org.springframework.beans.factory.annotation.Autowired;"
+          ""
+          "@RestController"
+          "public class {{controller-name}} {"
+          "    @Autowired"
+          "    {{service-class-name)}} {{service-var-name}}"
+          ""
+          "{{requests}}"
+          "}"]
+         (string/join \newline)
+         (<<)))
   )
