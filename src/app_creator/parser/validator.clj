ead9d5fe37965af488aa0ca3cc8c5d811c0eb2f4
Ekaterina Golikova <moskva20013@gmail.com>
2022-03-30 22:45:23 +0300

    Added deps list

--- src/app_creator/parser/validator.clj
+++ src/app_creator/parser/validator.clj
@@ -11,6 +11,25 @@
 (def method-name-regex #"^[a-z][\w]*$")
 (def uri-regex #"^(\/{1}\w+)+$")
 
+(def deps ["activemq" "actuator" "amqp" "artemis" "azure-active-directory" "azure-cosmos-db" "azure-keyvault-secrets"
+           "azure-storage" "azure-support" "batch" "cache" "camel" "cloud-bus" "cloud-cloudfoundry-discovery"
+           "cloud-config-client" "cloud-config-server" "cloud-contract-stub-runner" "cloud-contract-verifier"
+           "cloud-eureka" "cloud-eureka-server" "cloud-feign" "cloud-function" "cloud-gateway" "cloud-gcp"
+           "cloud-gcp-pubsub" "cloud-gcp-storage" "cloud-loadbalancer" "cloud-resilience4j" "cloud-starter"
+           "cloud-starter-consul-config" "cloud-starter-consul-discovery" "cloud-starter-sleuth"
+           "cloud-starter-vault-config" "cloud-starter-zipkin" "cloud-starter-zookeeper-config"
+           "cloud-starter-zookeeper-discovery" "cloud-stream" "cloud-task" "codecentric-spring-boot-admin-client"
+           "codecentric-spring-boot-admin-server" "configuration-processor" "data-cassandra" "data-cassandra-reactive"
+           "data-couchbase" "data-couchbase-reactive" "data-elasticsearch" "data-jdbc" "data-jpa" "data-ldap"
+           "data-mongodb" "data-mongodb-reactive" "data-neo4j" "data-r2dbc" "data-redis" "data-redis-reactive"
+           "data-rest" "data-rest-explorer" "datadog" "db2" "derby" "devtools" "flapdoodle-mongo" "flyway" "freemarker"
+           "geode" "graphite" "graphql" "groovy-templates" "h2" "hateoas" "hsql" "influx" "integration" "jdbc" "jersey"
+           "jooq" "kafka" "kafka-streams" "liquibase" "lombok" "mail" "mariadb" "mustache" "mybatis" "mysql" "native"
+           "new-relic" "oauth2-client" "oauth2-resource-server" "okta" "open-service-broker" "oracle" "picocli"
+           "postgresql" "prometheus" "quartz" "restdocs" "rsocket" "scs-config-client" "scs-service-registry" "security"
+           "session" "solace" "sqlserver" "testcontainers" "thymeleaf" "unboundid-ldap" "vaadin" "validation"
+           "wavefront" "web" "web-services" "webflux" "websocket"])
+
 (defn restrict-enum [elems & {:keys [in-work] :or {in-work false}}]
   "Создает перечисление со всеми возможными значениями для malli, а также сообщение об ошибке"
   (let [error-message (as-> elems e
@@ -19,9 +38,6 @@
                             (str "should be one of:\n- " e))
         enum (map str elems)]
     (vec (concat [:enum {:error/message error-message}] enum))))
-
-(defn matches [str regex]
-  (and (string? str) (re-matches regex str)))
 
 (def db-schema
   [:db
@@ -64,7 +80,7 @@
       [:java-version {:optional true} (restrict-enum ["1.8" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17"])]
       [:version {:optional true} string?]
       [:deps
-       [:sequential string?]]]]                             ; todo; some deps must be default
+       [:sequential (restrict-enum deps)]]]]                             ; todo; some deps must be default
     [:properties
      [:map
       [:db
