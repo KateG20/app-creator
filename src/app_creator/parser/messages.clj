c02b010dc64a1f24f29faa2599c5e02feb8b419a
Ekaterina Golikova <moskva20013@gmail.com>
2022-04-01 13:27:05 +0300

    Moved error messages to separate namespace

---
+++ src/app_creator/parser/messages.clj
@@ -0,0 +1,7 @@
+(ns app-creator.parser.messages)
+
+(def host-error "invalid ip-address or host name")
+(def port-error "invalid port number")
+(def controller-name-error "incorrect controller class name")
+(def method-name-error "incorrect method name")
+(def uri-path-error "incorrect path")
