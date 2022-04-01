b7a10dc72971e9c5ad10548e7a30c5d283c5dc85
Ekaterina Golikova <moskva20013@gmail.com>
2022-04-02 00:52:43 +0300

    Trying to get path to erroneous place with dfs

--- src/app_creator/parser/parser.clj
+++ src/app_creator/parser/parser.clj
@@ -1,13 +1,17 @@
 (ns app-creator.parser.parser)
 
 (require '[clj-yaml.core :as yaml]
-         '[app-creator.parser.validator :as validator])
+         '[app-creator.parser.validator :as validator]
+         '[malli.provider :as mp])
 
-(defn parse-from-file [path]
+(defn parse-from-file [path]                                ; todo https://github.com/metosin/malli#dot визуализация
   ; https://github.com/metosin/malli
   (let [text (yaml/parse-string (slurp path))
         errors (validator/explain text)]
     (clojure.pprint/pprint text)
+    ;(println "\nPROVIDEDSTART\n")
+    ;(clojure.pprint/pprint (mp/provide text))
+    ;(println "\nPROVIDEDEND\n")
     (if errors
       (clojure.pprint/pprint errors)
       (println "YAML IS VALID"))
