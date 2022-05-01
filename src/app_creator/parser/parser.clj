b1570537d1b1fb136b4bb27b7af3582152773786
Ekaterina Golikova <moskva20013@gmail.com>
2022-05-01 12:03:28 +0300

    Handling error occurring when backslashes are in input file

--- src/app_creator/parser/parser.clj
+++ src/app_creator/parser/parser.clj
@@ -1,12 +1,22 @@
-(ns app-creator.parser.parser)
+(ns app-creator.parser.parser
+  (:import (org.yaml.snakeyaml.scanner ScannerException)))
 
 (require '[clj-yaml.core :as yaml]
-         '[app-creator.parser.validator :as validator])
+         '[app-creator.parser.validator :as validator]
+         '[app-creator.parser.messages :as m])
 
 (defn parse-from-file [path]                                ; todo https://github.com/metosin/malli#dot визуализация
   ; https://github.com/metosin/malli
-  (let [data (yaml/parse-string (slurp path))
-        errors (validator/explain data)]
 
-    {:errors errors :data data}))
+  (try
+    (let [data (yaml/parse-string (slurp path))
+          errors (validator/explain data)]
+      {:errors errors :data data})
+
+    (catch ScannerException sc-e
+      {:errors [m/jar-path-error] :data nil})
+
+    (catch Exception e
+      {:errors ["Something went wrong while parsing. Try again or contact us to solve issue"]}))
+  )
 
