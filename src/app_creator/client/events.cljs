84b076544e82dc38832b38bf28519c5a5cd9c093
Ekaterina Golikova <moskva20013@gmail.com>
2023-05-01 21:02:03 +0300

    Changed structure of initial db

--- src/app_creator/client/events.cljs
+++ src/app_creator/client/events.cljs
@@ -8,15 +8,17 @@
 (re-frame/reg-event-db                                      ;; sets up initial application state
   ::initialize
   (fn [_ _]                                                 ;; arguments not important, so use _
-    {:all-valid             true
-     :text                  "initial text"
+    {
+     ;:all-valid             true
+     ;:text                  "initial text"
      :error-display         "none"                          ;; returned value put into app-db
-     :server-framework-text "default"
-     :server-lang-text      "default"
+     ;:server-framework-text "default"
+     ;:server-lang-text      "default"
      :success-display       "none"
      :http-post-result-text "null info"
      ;:log-field-display     "none"
      :log-text              ""
+     ; to identify number of boxed and rows, and maybe to identify data?
      :tables                [0 1]
      :table-columns         [[0 0] [0 1] [1 0]]
      :controllers           [0]
@@ -25,109 +27,159 @@
      :jar-conts             [0]
      :nginx-conts           [0]
      :postgres-conts        [0]
+
      :checked               {:db     "postgres"
                              :server {:type   "spring"
-                                      :build  "gradle"
-                                      :lang   "java"
-                                      :pack   "jar"
-                                      :boot-v "3.0.6"
-                                      :java-v "17"}
-                             :client {:type "android"
-                                      :lang "java"
-                                      :test "junit-jupiter"}
+                                      :spring {:build  "gradle"
+                                               :lang   "java"
+                                               :pack   "jar"
+                                               :boot-v "3.0.6"
+                                               :java-v "17"}}
+                             :client {:type    "android"
+                                      :android {:lang "java"
+                                                :test "junit-jupiter"}}
                              :deploy "docker"}
      :valid                 {:db       {:host true}
                              :server   {:db-host true}
                              :out-path true}
      :data                  {:db
-                             {:postgresql
-                              {:db-name  nil,
-                               :host     nil,
-                               :username nil,
-                               :password nil,
-                               :tables   nil
-                               ;[{:table-name "t1",
-                               ;  :columns
-                               ;  ({:col-name "c1", :opts "string"}
-                               ;   {:col-name "c2", :opts "number"}
-                               ;   {:col-name "c3", :opts "date"})}
-                               ; {:table-name "t2", :columns
-                               ;  ({:col-name "c1", :opts "bool"})}]
-                               }},
+                             {:type  "postgres"
+                              :value {:db-name  {:value ""
+                                                 :valid true}
+                                      :host     {:value ""
+                                                 :valid true}
+                                      :username {:value ""
+                                                 :valid true}
+                                      :password {:value ""
+                                                 :valid true}
+                                      :tables   {:table-vec  [0 1]
+                                                 :column-vec [[0 0] [0 1] [1 0]]
+                                                 :content    {0 {:name    {:value "t1"
+                                                                           :valid true}
+                                                                 :columns {0 {:name {:value "c1"
+                                                                                     :valid true}
+                                                                              :opts {:value "str"
+                                                                                     :valid true}}
+                                                                           1 {:name {:value "c2"
+                                                                                     :valid true}
+                                                                              :opts {:value "int"
+                                                                                     :valid true}}}}
+                                                              1 {:name    {:value "t2"
+                                                                           :valid true}
+                                                                 :columns {0 {:name {:value "c1"
+                                                                                     :valid true}
+                                                                              :opts {:value "date"
+                                                                                     :valid true}}}}}}}}
                              :server
-                             {:spring
+                             {:type "spring"
+                              :value
                               {:project
-                               {:build        "maven",
-                                :language     "java",
-                                :boot-version "2.6.6",
-                                :group        "com.example",
-                                :artifact     "demo_proj",
-                                :proj-name    "my_demo_project",
-                                :description  "demo_description",
-                                :packaging    "jar",
-                                :java-version "11",
+                               {:build        "gradle"
+                                :language     "java"
+                                :boot-version "3.0.6"
+                                :group        {:value ""
+                                               :valid true}
+                                :artifact     {:value ""
+                                               :valid true}
+                                :proj-name    {:value ""
+                                               :valid true}
+                                :description  {:value ""
+                                               :valid true}
+                                :packaging    "jar"
+                                :java-version "17"
                                 ;:project-version "0.0.1-SNAPSHOT",
                                 ;:deps            ("security")
                                 },
                                :properties
                                {:db
-                                {:type     "postgresql",
-                                 :username nil,
-                                 :password nil,
-                                 :sql-host nil,
-                                 :sql-port nil,
-                                 :db-name  nil}},
-                               :controllers nil
-                               ;[{:controller-name nil,
-                               ;  :requests
-                               ;  [{:req-name "login", :uri "/login", :mapping "get"}
-                               ;   {:req-name "deleteUser", :uri "/user/delete", :mapping "delete"}
-                               ;   {:req-name "createUser", :uri "/user/create", :mapping "post"}]}
-                               ; {:controller-name "ShoutController",
-                               ;  :requests
-                               ;  [{:req-name "shout", :uri "/shout", :mapping "post"}
-                               ;   {:req-name "findAll", :uri "/shouts", :mapping "get"}]}]
-                               }},
+                                {:type     {:value ""
+                                            :valid true}
+                                 :username {:value ""
+                                            :valid true}
+                                 :password {:value ""
+                                            :valid true}
+                                 :sql-host {:value ""
+                                            :valid true}
+                                 :sql-port {:value ""
+                                            :valid true}
+                                 :db-name  {:value ""
+                                            :valid true}}}
+                               :controllers {:controller-vec [0]
+                                             :method-vec     [[0 0] [0 1]]
+                                             :content        {0 {:name    {:value "UserController"
+                                                                           :valid true}
+                                                                 :methods {0 {:name {:value "m1"
+                                                                                     :valid true}
+                                                                              :url  {:value "/user"
+                                                                                     :valid true}
+                                                                              :type {:value "post"
+                                                                                     :valid true}}
+                                                                           1 {:name {:value "m1"
+                                                                                     :valid true}
+                                                                              :url  {:value "/user"
+                                                                                     :valid true}
+                                                                              :type {:value "post"
+                                                                                     :valid true}}}}}}}},
                              :client
-                             {:android
-                              {:proj-name      nil,
-                               :language       nil,
-                               :package-name   nil,
-                               :test-framework nil,
-                               :server-host    nil,
-                               :server-port    nil,
-                               :requests       nil
-                               ;[{:req-name "getUser", :uri "/user", :type "get", :entity "User"}
-                               ; {:req-name "postShout",
-                               ;  :uri      "/shout",
-                               ;  :type     "post",
-                               ;  :entity   "Shout"}]
-                               }},
+                             {:type "android"
+                              :value
+                              {:proj-name      {:value ""
+                                                :valid true}
+                               :language       "java"
+                               :package-name   {:value ""
+                                                :valid true}
+                               :test-framework "junit-jupiter"
+                               :server-host    {:value ""
+                                                :valid true}
+                               :server-port    {:value ""
+                                                :valid true}
+                               :endpoints      {:endpoints-vec [0 1]
+                                                :content       {0 {:name   {:value ""
+                                                                            :valid true}
+                                                                   :url    {:value ""
+                                                                            :valid true}
+                                                                   :type   {:value ""
+                                                                            :valid true}
+                                                                   :entity {:value ""
+                                                                            :valid true}}
+                                                                1 {:name   {:value ""
+                                                                            :valid true}
+                                                                   :url    {:value ""
+                                                                            :valid true}
+                                                                   :type   {:value ""
+                                                                            :valid true}
+                                                                   :entity {:value ""
+                                                                            :valid true}}}}}}
                              :containerization
-                             {:docker
-                              {:jars
-                               [{:image-name     nil,
-                                 :container-name nil,
-                                 :dir-name       nil,
-                                 :jar-path       nil}
-                                ;{:image-name     "second-image",
-                                ; :container-name "second-container",
-                                ; :dir-name       "second-dir",
-                                ; :jar-path
-                                ; "C:/Users/Lenovo X1/Downloads/kursach files/first.jar"}
-                                ],
-                               :nginx
-                               [{:image-name             nil,
-                                 :container-name         nil,
-                                 :dir-name               nil,
-                                 :backend-container-name nil}
-                                ;{:image-name             "second-nginx",
-                                ; :container-name         "second-nginx-c",
-                                ; :dir-name               "second-nginx-dir",
-                                ; :backend-container-name "does-not-matter"}
-                                ],
-                               :postgres [{:container-name nil, :port nil, :password nil}],
-                               :network  {:network-name nil}}}
+                             {:type "docker"
+                              :value
+                              {:network  {:value ""
+                                          :valid true}
+                               :jars     {:cont-vec [0]
+                                          :content  {0 {:image-name     {:value ""
+                                                                         :valid true}
+                                                        :container-name {:value ""
+                                                                         :valid true}
+                                                        :dir-name       {:value ""
+                                                                         :valid true}
+                                                        :jar-path       {:value ""
+                                                                         :valid true}}}}
+                               :nginx    {:cont-vec [0]
+                                          :content  {0 {:image-name             {:value ""
+                                                                                 :valid true}
+                                                        :container-name         {:value ""
+                                                                                 :valid true}
+                                                        :dir-name               {:value ""
+                                                                                 :valid true}
+                                                        :backend-container-name {:value ""
+                                                                                 :valid true}}}}
+                               :postgres {:cont-vec [0]
+                                          :content  {0 {:container-name {:value ""
+                                                                         :valid true}
+                                                        :port           {:value ""
+                                                                         :valid true}
+                                                        :password       {:value ""
+                                                                         :valid true}}}}}}
                              :out-path nil}
      }))                                                    ; https://day8.github.io/re-frame/dominoes-live/#initialize
 
@@ -166,6 +218,9 @@
               ;:http-post-result-text (str "failure! " result)
               :log-text "Something went wrong!"
               )))
+
+(defn find-not-valid [db]
+  )
 
 ; :params - "GET will add params onto the query string, POST will put the params in the body"
 ; but there are also :body and :url-params.
@@ -280,37 +335,37 @@
 (re-frame/reg-event-db
   ::change-spring-build
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :server :build] new-value)))
+    (assoc-in db [:checked :server :spring :build] new-value)))
 
 (re-frame/reg-event-db
   ::change-spring-lang
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :server :lang] new-value)))
+    (assoc-in db [:checked :server :spring :lang] new-value)))
 
 (re-frame/reg-event-db
   ::change-spring-pack
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :server :pack] new-value)))
+    (assoc-in db [:checked :server :spring :pack] new-value)))
 
 (re-frame/reg-event-db
   ::change-spring-boot-v
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :server :boot-v] new-value)))
+    (assoc-in db [:checked :server :spring :boot-v] new-value)))
 
 (re-frame/reg-event-db
   ::change-spring-java-v
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :server :java-v] new-value)))
+    (assoc-in db [:checked :server :spring :java-v] new-value)))
 
 (re-frame/reg-event-db
   ::change-android-lang
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :client :lang] new-value)))
+    (assoc-in db [:checked :client :android :lang] new-value)))
 
 (re-frame/reg-event-db
   ::change-android-test
   (fn [db [_ new-value]]
-    (assoc-in db [:checked :client :test] new-value)))
+    (assoc-in db [:checked :client :android :test] new-value)))
 
 (re-frame/reg-event-db
   ::db-host-text-change
@@ -320,7 +375,8 @@
       (-> db
           (assoc-in [:data :db :postgresql :host] new-host-value)
           (assoc-in [:valid :db :host] is-valid)
-          (assoc db :all-valid is-valid)))))
+          ;(assoc db :all-valid is-valid)
+          ))))
 
 (re-frame/reg-event-db
   ::out-path-text-change
@@ -330,7 +386,7 @@
       (-> db
           (assoc-in [:data :out-path] new-path-value)
           (assoc-in [:valid :out-path] is-valid)
-          (assoc db :all-valid is-valid)
+          ;(assoc db :all-valid is-valid)                    ; треш. как можно быть такой тупой. так нельзя
           (assoc db :log-text (str is-valid))))))
 
 
