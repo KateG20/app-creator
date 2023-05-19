70cc1368dceaef6b29f768a64d5c2de450c52a8c
Kate Golikova <moskva20013@gmail.com>
2023-05-19 16:19:41 +0300

    Changed jdk image

--- src/app_creator/core/creator/containerization/docker/templates.clj
+++ src/app_creator/core/creator/containerization/docker/templates.clj
@@ -7,7 +7,7 @@
 
 (def sep File/separator)
 
-(def current-jdk-image "adoptopenjdk/openjdk11:jdk-11.0.11_9-alpine")
+(def current-jdk-image "eclipse-temurin:17.0.6_10-jdk")
 
 (defn jar-dockerfile [jar-path]
   (->> [
